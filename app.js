310d2f97ed778c19fb0efec0455cc67ff1614ca3
MarkLurton <marklurton@gmail.com>
2022-04-22 19:54:14 -0500

    Change url ref to HTTPS

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ $form[0].addEventListener("submit", async function (event) {
   event.preventDefault();
   const searchTerm = $input.val();
   const res = await axios.get(
-    `http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`
+    `https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`
   );
   const gifs = res.data.data;
   console.log(gifs);
